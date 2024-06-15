67346e2ded8e166e29d931c3f97eb01e0f83f2c9
zwpdbh <zw>
2024-06-15 14:37:12 +0800

    Leptos with bulma is setup

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,32 @@
 use leptos::*;
+use leptos_bulma::elements::{BAButton, BButton, BButtons};
+use leptos_bulma::enums::BColor;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world zhao!"</p> })
+    mount_to_body(|| {
+        view! { <div>
+        <p>"Hello, world zhao!"</p>
+            <BasicButtons/>
+        </div>  }
+    })
+}
+
+#[component]
+pub fn BasicButtons() -> impl IntoView {
+    view! {
+        <BButtons>
+            <BButton>"Normal button"</BButton>
+            <BAButton href="/elements#button" target="_blank" title="Open in a new tab">
+                "Anchor link button"
+            </BAButton>
+            <BButton is_light=true>"Light button"</BButton>
+            <BButton is_dark=true>"Dark button"</BButton>
+            <BButton is_inverted=true>"Inverted button"</BButton>
+            <BButton is_outlined=true color=BColor::Primary>
+                "Outlined button"
+            </BButton>
+            <BButton is_rounded=true>"Rounded button"</BButton>
+            <BButton is_fullwidth=true>"Fullwidth button"</BButton>
+        </BButtons>
+    }
 }
